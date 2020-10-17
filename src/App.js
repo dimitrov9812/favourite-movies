5eb2cf2b7afd797907a59a825047bac684a91e76
dimitrov9812 <alexanderdimitrov333@gmail.com>
2020-10-17 19:14:09 +0300

    setup react-router-dom and matched the versions to make webpack work withour any error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
+import Home from './components/home/Home';
+import Favourites from './components/favourites/Favourites';
 
 function App() {
   return (
-    <div className="App">
-      <h1>MY APPS</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/login" exact component={Login}/>
+        <Route path="/register" exact component={Register} />
+        <Route path="/home" exact component={Home} />
+        <Route path="/favourites" exact component={Favourites} />
+      </Switch>
+    </Router>
   );
 }
 
