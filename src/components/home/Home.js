05fcd4815133e47e126f3462c58cd56f9c58a971
dimitrov9812 <alexanderdimitrov333@gmail.com>
2020-10-19 20:51:37 +0300

    added env

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -69,7 +69,6 @@ const Home = () => {
                 <Button onClick={() => scrollTop()}>
                     Scroll to Top
                 </Button>
-                </Button>
                 <Button onClick={() => scrollTop()}>
                     {process.env.ACCESS_TOKEN}
                 </Button>
