7a92acca5f84ed1646dd79da37e554dc16e8dbf9
dimitrov9812 <alexanderdimitrov333@gmail.com>
2020-11-04 13:15:08 +0200

    Responsive mobile changes

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -5,11 +5,22 @@ import Nav from '../../navigation/Nav';
 import { RootStoreContext } from '../../store/RootStore';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 import TheatersIcon from '@material-ui/icons/Theaters';
+import useForceUpdate from 'use-force-update';
 
 const Details = ({ history }) => {
     const store = useContext(RootStoreContext);
     const [movies, setMovies] = useState({});
     const [loading, setLoading] = useState(false);
+    const forceUpdate = useForceUpdate();
+    const width = window.innerWidth;
+    
+    useEffect(() => {
+        window.addEventListener('resize', resize);
+
+        return () => {
+            window.removeEventListener('resize', resize);
+          }
+    },[])
 
     useEffect(() => {
         setLoading(true);
@@ -25,6 +36,8 @@ const Details = ({ history }) => {
         });
     }, [store.userStore])
 
+    const resize = () => forceUpdate();
+
     const scrollTop = async () => {
         window.location.reload();
     };
@@ -37,7 +50,7 @@ const Details = ({ history }) => {
     const handleYouTubeSearch = () => {
         window.open(`https://www.youtube.com/results?search_query=${store.userStore.movies.title}+movie+trailer`, '_blank');
     }
-    const renderMovieDetails = () => {
+    const renderMovieDetails = (imageSize) => {
         return (
             <div style={{ display: 'flex', flexDireciton: 'column', padding:20}}>
                 <Card>
@@ -45,7 +58,7 @@ const Details = ({ history }) => {
                         <CardMedia
                             title={store.userStore.movies.title}
                             style={{ justifyContent: 'center', alignItems: 'center', padding: 10, paddingTop:20, paddingRight: 20}}>
-                            <img className={store.userStore.nightMode ? 'darkImg' : null} src={`https://image.tmdb.org/t/p/w500${store.userStore.movies.poster_path}`} alt="recipe thumbnail" />
+                            <img style={{display:'flex', width:`${imageSize}`, margin:'0 auto'}} src={`https://image.tmdb.org/t/p/w${imageSize}${store.userStore.movies.poster_path}`} alt="recipe thumbnail" />
                         </CardMedia>
                     </CardActionArea>
                 </Card>
@@ -107,13 +120,99 @@ const Details = ({ history }) => {
             </div>
         )
     }
+    const checkMobile = () => {
+        if (width < 1226) {
+            return renderMobileDetails();
+        }
+        if (width > 1226 && width < 1500) {
+            return renderTabletDetails();
+        }
+        return renderDesktopDetails();
+    }
+
+    const renderMobileDetails = () => {
+        return (
+            <div style={{padding:20}}>
+                <Card>
+                    <CardActionArea>
+                        <CardMedia
+                            title={store.userStore.movies.title}
+                            style={{ justifyContent: 'center', alignItems: 'center', padding: 10, paddingTop:20, paddingRight: 20}}>
+                            <img style={{display:'flex', width:300, margin:'0 auto'}} src={`https://image.tmdb.org/t/p/w300${store.userStore.movies.poster_path}`} alt="recipe thumbnail" />
+                        </CardMedia>
+                    </CardActionArea>
+                    <div>
+                            <div>
+                                <CardContent>
+                                    <Typography  variant="body2" color="textSecondary" component="p">
+                                        <h4>Original Title</h4>{store.userStore.movies.title}
+                                    </Typography>
+                                </CardContent>
+                            </div>
+                            <div>
+                                <CardContent>
+                                    <Typography  variant="body2" color="textSecondary" component="p">
+                                        <h4>Average vote</h4>{store.userStore.movies.vote_average}
+                                    </Typography>
+                                </CardContent>
+                            </div>
+                            <div>
+                                <CardContent>
+                                    <Typography  variant="body2" color="textSecondary" component="p">
+                                        <h4>Genre</h4>{store.userStore.movies.genres? store.userStore.movies.genres[0].name : "Can't get genre"}
+                                    </Typography>
+                                </CardContent>
+                            </div>
+                            <div>
+                                <CardContent>
+                                    <Typography  variant="body2" color="textSecondary" component="p">
+                                        <h4>Release Date</h4>{store.userStore.movies.release_date}
+                                    </Typography>
+                                </CardContent>
+                            </div>
+                            <div>
+                                <CardContent>
+                                    <Typography  variant="body2" color="textSecondary" component="p">
+                                        <h4>Overview</h4>{store.userStore.movies.overview}
+                                    </Typography>
+                                </CardContent>
+                                <CardContent style={{display:'flex',flexDirection:'row'}}>
+                                    <Typography  variant="body2" color="textSecondary" component="p">
+                                        <div style={{display:'flex', flexDirection:'column', cursor:'pointer'}} onClick={() => handleYouTubeSearch()}>
+                                            <span style={{marginLeft:14}}>See Trailer</span>
+                                            <YouTubeIcon style={{color:'red', fontSize:100}}></YouTubeIcon>
+                                        </div>
+                                    </Typography>
+                                    <Typography  variant="body2" color="textSecondary" component="p">
+                                        <div style={{display:'flex', flexDirection:'column', cursor:'pointer'}} onClick={() => handleImdbSearch()}>
+                                            <span style={{marginLeft:35}}>IMDB</span>
+                                            <TheatersIcon style={{color:'black', fontSize:100, rotate:'90deg'}}></TheatersIcon>
+                                        </div>
+                                    </Typography>
+                                </CardContent>
+                            </div>
+                        </div>
+                </Card>
+            </div>
+        )
+    }
+
+    const renderDesktopDetails = () => {
+        return renderMovieDetails(500);
+    }
+
+    const renderTabletDetails = () => {
+        return renderMovieDetails(300)
+    }
+
 
     return (
         <div>
             <Nav />
             <div style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', backgroundColor: '#e8e7e3',height: '93vh'}}>
                 <div style={{ display: 'flex', flexDirection: 'column', width: '70%', margin: '0 auto' }}>
-                    {renderMovieDetails()}
+                    {/* {renderMovieDetails()} */}
+                    {checkMobile()}
                 </div>
             </div>
         </div>
--- src/navigation/Nav.js
+++ src/navigation/Nav.js
@@ -1,5 +1,5 @@
 import { Button, FormControlLabel, Paper, Switch, Tab, Tabs, TextField } from '@material-ui/core';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { RootStoreContext } from '../store/RootStore';
 import SearchIcon from '@material-ui/icons/Search';
@@ -7,12 +7,31 @@ import Brightness2Icon from '@material-ui/icons/Brightness2';
 import useForceUpdate from 'use-force-update';
 import Cookie from 'js-cookie';
 import { useHistory } from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Nav = () => {
     const store = useContext(RootStoreContext);
     const [filter, setFilter] = useState('');
+    const [hiddenMenu, setHiddenMenu] = useState(false);
     const forceUpdate = useForceUpdate();
     const history = useHistory();
+    const width = window.innerWidth;
+    
+
+    useEffect(() => {
+        window.addEventListener('resize', resize);
+
+        return () => {
+            window.removeEventListener('resize', resize);
+          }
+    },[])
+
+    useEffect(() => {
+        checkMobile();
+        forceUpdate();
+    }, [window.innerWidth]);
+
+    const resize = () => forceUpdate();
 
     const handleSearch = () => {
         console.log(filter);
@@ -22,37 +41,74 @@ const Nav = () => {
             forceUpdate();
         }
     }
-    return (
-        <div className="App">
-            <Paper square>
-                <Tabs indicatorColor="primary"
-                    textColor="primary"
-                    onChange={(e) => console.log(e.target.value)}
-                    aria-label="disabled tabs example">
-                    <div style={{ width: '100%', backgroundColor: '#cccccc', }}>
-                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', margin: "0 auto", width: '80%' }}>
-                            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                <Link to="/home" onClick={async () => await store.userStore.getMovies("top-rated")}><Tab label="Home"></Tab></Link>
-                                <Link to="/popular" onClick={async () => await store.userStore.getMovies("popular")}><Tab label="Popular"></Tab></Link>
-                                <Link to="/upcoming" onClick={async () => await store.userStore.getMovies("upcoming")}><Tab label="Upcomi ng"></Tab></Link>
-                            </div>
-                            <div style={{ display: 'flex', alignItems: 'center', alignSelf: 'flex-end' }}>
-                                <TextField style={{ marginBottom: 15 }} id="standard-basic" label="Enter movie keyword" onChange={(e) => setFilter(e.target.value)} />
-                                {/* <Link to={`/search/:${filter}`}><Button
+
+    const renderHiddenMenu = () => {
+        if (hiddenMenu) {
+            return (
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor:'#cccccc'}}>
+                    <Link to="/home" onClick={async () => await store.userStore.getMovies("top-rated")}><Tab label="Home"></Tab></Link>
+                    <Link to="/popular" onClick={async () => await store.userStore.getMovies("popular")}><Tab label="Popular"></Tab></Link>
+                    <Link to="/upcoming" onClick={async () => await store.userStore.getMovies("upcoming")}><Tab label="Upcomi ng"></Tab></Link>
+                    <Link to="/random" onClick={() => console.log("random")}><Tab label="Random"></Tab></Link>
+                    <Link onClick={() => console.log("logging out")}><Tab label="Logout"></Tab></Link>
+                </div>
+            )
+        }
+    }
+    const checkMobile = () => {
+        if (width < 1226) {
+            return renderMobileNav();
+        }
+        return renderDesktopNav();
+    }
+    const renderMobileNav = () => {
+        return (
+            <div>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', backgroundColor: '#cccccc' }}>
+                    <MenuIcon onClick={() => setHiddenMenu(!hiddenMenu)} style={{ fontSize: '50px', cursor: 'pointer' }}></MenuIcon>
+                    <TextField style={{ marginBottom: 15 }} id="standard-basic" label="Enter movie keyword" onChange={(e) => setFilter(e.target.value)} />
+                    <SearchIcon style={{ cursor: 'pointer', fontSize: '50px' }} onClick={() => handleSearch()}>Search</SearchIcon>
+                </div>
+                {hiddenMenu ? renderHiddenMenu() : null}
+            </div>
+        )
+    }
+    const renderDesktopNav = () => {
+        return (
+            <div className="App">
+                <Paper square>
+                    <Tabs indicatorColor="primary"
+                        textColor="primary"
+                        onChange={(e) => console.log(e.target.value)}
+                        aria-label="disabled tabs example">
+                        <div style={{ width: '100%', backgroundColor: '#cccccc', }}>
+                            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', margin: "0 auto", width: '80%' }}>
+                                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                    <Link to="/home" onClick={async () => await store.userStore.getMovies("top-rated")}><Tab label="Home"></Tab></Link>
+                                    <Link to="/popular" onClick={async () => await store.userStore.getMovies("popular")}><Tab label="Popular"></Tab></Link>
+                                    <Link to="/upcoming" onClick={async () => await store.userStore.getMovies("upcoming")}><Tab label="Upcomi ng"></Tab></Link>
+                                </div>
+                                <div style={{ display: 'flex', alignItems: 'center', alignSelf: 'flex-end' }}>
+                                    <TextField style={{ marginBottom: 15 }} id="standard-basic" label="Enter movie keyword" onChange={(e) => setFilter(e.target.value)} />
+                                    {/* <Link to={`/search/:${filter}`}><Button
                                     variant="contained"
                                     color="secondary"
                                     size="small"
                                     style={{ margin: 10 }}
                                     startIcon={<SearchIcon />}>Search</Button></Link> */}
-                                <SearchIcon onClick={() => handleSearch()}>Search</SearchIcon>
-                                <Link to="/random" onClick={() => console.log("random")}><Tab label="Random"></Tab></Link>
-                                <Link onClick={() => console.log("logging out")}><Tab label="Logout"></Tab></Link>
+                                    <SearchIcon style={{ cursor: 'pointer' }} onClick={() => handleSearch()}>Search</SearchIcon>
+                                    <Link to="/random" onClick={() => console.log("random")}><Tab label="Random"></Tab></Link>
+                                    <Link onClick={() => console.log("logging out")}><Tab label="Logout"></Tab></Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </Tabs>
-            </Paper>
-        </div>
+                    </Tabs>
+                </Paper>
+            </div>
+        )
+    }
+    return (
+        checkMobile()
     )
 }
 
